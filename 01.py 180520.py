659f018d3bd38694115079a3020d133e62fceebd
xp1978594666 <xp1978594666@xp1978594666.com>
2018-06-06 02:51:34 +0800

    180606 2.5

--- 01.py 180520.py
+++ 01.py 180520.py
@@ -33,9 +33,4 @@ print(yueyue.age）
 # 注意成员函数的调用没有传入进参数
  yueyue.doHomework()
 
-# 3. anaconda基本使用
- - anaconda主要是一个虚拟环境管理器
- - 还是一个安装包管理器
- - conda list: 显示anaconda安装的包
-- conda env list: 显示anaconda的虚拟环境列表
-- conda create -n xxx python=3.6: 创建Python版本为3.6的虚拟环境，名称为xxx
+
